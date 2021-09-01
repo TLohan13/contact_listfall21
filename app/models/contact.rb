dd4b28b62cd488a0e1134808d6db59292f361208
Thomas Lohan <johndoe@example.com>
2021-08-31 23:00:34 -0400

    end of models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,20 @@
 class Contact < ApplicationRecord
+  # Associations - describing relatiosnhips
+  # belongs_to
+  # has_many
+  # has_one
+  # has_many :through
+  #   dependent 
+
+  has_many :notes, dependent: :destroy 
+  has_one :address, dependent: :destroy
+
+  #Validations 
+  # - Model Validations, stop in the model to validate data going into the database
+  validates :first_name, :last_name, :email, presence: true
+
+  validates :age, numericality: true 
+  validates :email, uniqueness: true
+
+  
 end
